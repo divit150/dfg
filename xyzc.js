20b4c9ac15ac60ddd50785f0f9fe70ea0534841b
divit150 <77146400+divit150@users.noreply.github.com>
2021-08-08 12:47:25 +0530

    Add files via upload

---
+++ xyzc.js
@@ -0,0 +1,28 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(250, 250);
+    canvas.center();
+    video=createCapture(VIDEO);
+ video.size(250, 250);
+ video.hide();
+}
+
+function te() {
+    save("my_picture.png");
+}
+function draw(){
+    image(video,0,0,250,250);
+}
+
+function modelLoaded() {
+    console.log('poseNetStarted');
+}
+
+function getPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose x= "+results[0].pose.nose.x);
+        console.log("nose y= "+results[0].pose.nose.y);
+        }
+}
